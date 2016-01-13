1e947a82bb62ec06765134ddb606c0d0790592b7
Benjamin Pachev <benjaminpachev@gmail.com>
2016-01-12 21:10:36 -0700

    Basic console I/O working.

--- EvilHangman/Main.java
+++ EvilHangman/Main.java
@@ -1,10 +1,15 @@
-
+import java.io.Console;
 
 public class Main {
 
 	public static void main(String[] args)
 	{
-		
+		Console c = System.console();
+		while (true)
+		{
+			String nguess = c.readLine("Enter Next Guess: ");
+			System.out.println("You entered " + nguess + " , a string of length " + nguess.length());
+		}
 	}
 
 }
