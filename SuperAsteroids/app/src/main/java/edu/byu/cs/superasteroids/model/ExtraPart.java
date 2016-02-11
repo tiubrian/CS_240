dacd4d5072e43d76a0805df4818dc28851708ed2
Benjamin Pachev <benjaminpachev@gmail.com>
2016-02-10 20:10:25 -0700

    Added extraPart

---
+++ SuperAsteroids/app/src/main/java/edu/byu/cs/superasteroids/model/ExtraPart.java
@@ -0,0 +1,11 @@
+package edu.byu.cs.superasteroids.model;
+
+/**
+ * A part of the spaceship
+ * Really just the left wing, but for consistency its called extra part.
+ */
+public class ExtraPart
+{
+	public GameImage image;
+  public Coordinate attachPoint;
+}
